05147fe05c3942b2ed3404ce18396a80551b1fb0
Tristan Slater <hello@trslater.ca>
2022-06-02 10:26:31 -0700

    Stream image blocks as base64 encoded strings

--- jpglab/api.py
+++ jpglab/api.py
@@ -1,22 +1,28 @@
+from base64 import b64encode
+
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import StreamingResponse
 from PIL import Image
 
 app = FastAPI()
 
+BLOCK_SIZE = 8
+
 
 @app.post("/upload")
 async def upload(upload: UploadFile):
     image = Image.open(upload.file)
+    # DEBUG: Shrink for now, so a predictable number of blocks are generated
+    image = image.resize((64, 64))
 
-    block_size = 400
-    for i in range(image.height//block_size):
-        for j in range(image.width//block_size):
+    blocks = (
+        b64encode(
             image.crop((
-                j*block_size,
-                i*block_size,
-                (j + 1)*block_size,
-                (i + 1)*block_size)).show()
+            j*BLOCK_SIZE,
+            i*BLOCK_SIZE,
+            (j + 1)*BLOCK_SIZE,
+            (i + 1)*BLOCK_SIZE)).tobytes()).decode("ascii")
+        for i in range(image.height//BLOCK_SIZE)
+        for j in range(image.width//BLOCK_SIZE))
 
-    return StreamingResponse(iter(
-        ("OSUDJCIQ", "BVUQODLK", "SUDHQBVY", "ZXOQPWOI", "IUWRVCBN")))
+    return StreamingResponse(blocks)
