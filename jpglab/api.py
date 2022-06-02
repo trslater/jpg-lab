b29df2bc9a63237b6dbfd7bd6fa50bb9a72d52ea
Tristan Slater <hello@trslater.ca>
2022-06-02 10:15:23 -0700

    Chop image up into blocks

--- jpglab/api.py
+++ jpglab/api.py
@@ -9,7 +9,14 @@ app = FastAPI()
 async def upload(upload: UploadFile):
     image = Image.open(upload.file)
 
-    image.show()
+    block_size = 400
+    for i in range(image.height//block_size):
+        for j in range(image.width//block_size):
+            image.crop((
+                j*block_size,
+                i*block_size,
+                (j + 1)*block_size,
+                (i + 1)*block_size)).show()
 
     return StreamingResponse(iter(
         ("OSUDJCIQ", "BVUQODLK", "SUDHQBVY", "ZXOQPWOI", "IUWRVCBN")))
