475fe167f82a9b8a7916534d4194c95bccf3c7c8
Tristan Slater <hello@trslater.ca>
2022-06-02 10:13:46 -0700

    Get basic image upload working

--- jpglab/api.py
+++ jpglab/api.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 from fastapi.responses import StreamingResponse
+from PIL import Image
 
 app = FastAPI()
 
 
 @app.post("/upload")
-async def upload():
+async def upload(upload: UploadFile):
+    image = Image.open(upload.file)
+
+    image.show()
+
     return StreamingResponse(iter(
         ("OSUDJCIQ", "BVUQODLK", "SUDHQBVY", "ZXOQPWOI", "IUWRVCBN")))
