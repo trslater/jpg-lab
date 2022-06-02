a661fae98ec90a9989d2c901b4f92f9ee3262120
Tristan Slater <hello@trslater.ca>
2022-06-02 11:40:15 -0700

    Add image block endpoint
    Also refactor block generation code and limit number of blocks wide (cols)

--- jpglab/api.py
+++ jpglab/api.py
@@ -1,4 +1,5 @@
-from base64 import b64encode
+from base64 import b64decode, b64encode
+from io import BytesIO
 
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import StreamingResponse
@@ -6,23 +7,43 @@ from PIL import Image
 
 app = FastAPI()
 
+MAX_COLS = 32
 BLOCK_SIZE = 8
 
 
 @app.post("/upload")
 async def upload(upload: UploadFile):
     image = Image.open(upload.file)
-    # DEBUG: Shrink for now, so a predictable number of blocks are generated
-    image = image.resize((64, 64))
+    if image.width//8 > MAX_COLS:
+        # Shrink to manageable size
+        image = image.resize((
+            MAX_COLS*BLOCK_SIZE,
+            int(MAX_COLS*BLOCK_SIZE*image.height/image.width)))
+    # Crop to perfect multiples of 8
+    image = image.crop((0, 0, 8*(image.width//8), 8*(image.height//8)))
 
-    blocks = (
-        b64encode(
-            image.crop((
-            j*BLOCK_SIZE,
-            i*BLOCK_SIZE,
-            (j + 1)*BLOCK_SIZE,
-            (i + 1)*BLOCK_SIZE)).tobytes()).decode("ascii")
-        for i in range(image.height//BLOCK_SIZE)
-        for j in range(image.width//BLOCK_SIZE))
+    return StreamingResponse(blocks(image))
 
-    return StreamingResponse(blocks)
+
+def blocks(image):
+    # j comes first, so we get blocks row-by-row
+    for i in range(image.height//BLOCK_SIZE):
+        for j in range(image.width//BLOCK_SIZE):
+            block = image.crop((
+                j*BLOCK_SIZE,
+                i*BLOCK_SIZE,
+                (j + 1)*BLOCK_SIZE,
+                (i + 1)*BLOCK_SIZE))
+
+            yield b64encode(block.tobytes())
+
+
+@app.get("/block.png")
+async def block(s: str):
+    image_bytes = b64decode(s)
+    image = Image.frombytes("RGB", (8, 8), image_bytes)
+    stream = BytesIO()
+    image.save(stream, format="png")
+    stream.seek(0)
+
+    return StreamingResponse(stream, media_type="image/png")
